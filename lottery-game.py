acb0d3626fd08add3d49362bb4591f92766268b2
Filip Livancic <filiplivancic@gmail.com>
2023-02-13 22:33:00 +0000

    restructured

--- lottery-game.py
+++ lottery-game.py
@@ -1,44 +1,62 @@
 import random as r
 
+def error_message(num,player_list):
+    if num < 1 or num > 50:
+        print('\nInvalid Input - Integer Must Be Between 1-50\n')
+    elif num in player_list:
+        print('\nInvalid Input - Cannot Have 2 Identical Picks\n')
+
+def selecting_first_numbers():
+
+    player_list = []
+    num_position = {
+        1:"First",
+        2:"Second",
+        3:"Third",
+        4:"Fourth",
+        5:"Fifth",
+        6:"Sixth"
+        }
+    num = 0 #Initialising variable outside of Integer Range For While Loop
+
+    for i in range(1,7):
+        while num < 1 or num > 50 or num in player_list or type(num) != int:
+            num = int(input(f"Enter Your {num_position.get(i)} Lottery Number: "))
+            error_message(num,player_list)
+        player_list.append(num)
+        num = 0 # Reset Variable
+
+    return player_list
+
+# def reroll(player_list,correct_values,incorrect_values):
+#     for i,val in enumerate(incorrect_values):
+
+
 def main():
 
-    # Generate your lottery numbers
-    player_list = []
-    num_position = ["First","Second","Third","Fourth","Fifth","Sixth"]
-    num = 0 #Initialisng variable to prevent while loop breaking
-
-    for pos in num_position:
-        while num < 1 or num > 50 or num in player_list or type(num) != int:
-            num = int(input(f"Enter Your {pos} Lottery Number: "))
-            if num < 1 or num > 50:
-                print('\nInvalid Input - Integer Must Be Between 1-50\n')
-            elif num in player_list:
-                print('\nInvalid Input - Cannot Have 2 Identical Picks\n')
-            elif type(num) != int:
-                print('\nInvalid Input - Must Be Integer Type\n')
-        player_list.append(num)
-        num = 0 # Resetting to while loop condition
-
+    player_list = selecting_first_numbers()
     print(f"Here are your Lottery Numbers: {player_list}")
 
     # Creating the Lottery Draw
     lottery_draw = r.sample(range(1,50),6)
 
-    print(f"Winning Lottery Draw: {lottery_draw}")
-
-    # Checking numbers for win condition
-    match_list = []
+    # Seperating Values into Correct/Incorrect Lists
+    correct_values = []
     incorrect_values = []
     for val in player_list:
         if val in lottery_draw:
-            match_list.append(val)
+            correct_values.append(val)
         elif val not in lottery_draw:
             incorrect_values.append(val)
 
-    if len(match_list) == 6:
-        print("Congrats! You are the Winner!")
-    else:
-        print(f"Unlucky... You only got {len(match_list)}/6 correct")
+    # Checking numbers for win condition
+    if len(correct_values) < 6:
+        print(f"\nUnlucky... You only got {len(correct_values)}/6 correct\n")
+        print(f"But you will have a chance to re-roll in the Bonus Round!")
+        # reroll()
+    elif len(correct_values) == 6:
+        print(f"The Winning Lottery Draw: {lottery_draw}")
+        print("\nCongrats! You are the Winner!\n")
 
 if __name__ == "__main__":
     main()
